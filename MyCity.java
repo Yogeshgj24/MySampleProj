b5a262b5f108ef9a2c4ba54c6015039cd9766968
Yogeshgj24 <101520390+Yogeshgj24@users.noreply.github.com>
2023-03-24 13:18:35 +0530

    Add files via upload

---
+++ MyCity.java
@@ -0,0 +1,11 @@
+package com;
+
+public class MyCity {
+
+	public static void main(String[] args) {
+		System.out.println("My city is Nashik.");
+		System.out.println("My city is Bangalore.");
+
+	}
+
+}
